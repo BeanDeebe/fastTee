b09ea5f0dfbfc7fea181de64b065ebc476034c3f
Dean Beebe <beebe.d@northeastern.edu>
2023-12-31 18:25:56 -0500

    Submit page now adds user to database

--- fastteefrontend/src/Pages/SignUp/SignUp.js
+++ fastteefrontend/src/Pages/SignUp/SignUp.js
@@ -1,5 +1,120 @@
 import React from "react";
+import useForm from "../../hooks/UseForm";
+import {Link} from "react-router-dom";
+import Welcome from "../Welcome/Welcome";
 
 export default function SignUp() {
-    return <h1>Sign Up Page</h1>
+
+    const {handleSubmit, status, message} = useForm({});
+
+    if (status === 'error') {
+        console.log(message);
+        console.log(status);
+        return (
+            <div>
+                <Link to='/' component={<Welcome />} />
+            </div>
+        )
+    }
+
+    if (status === 'success') {
+        console.log(message);
+        console.log('status', status);
+        return (
+            <div>
+                <Link to='/' component={<Welcome />} />
+            </div>
+        )
+    }
+    return (
+        <>
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h2 className="mt-10 text-center text-3xl font-bold leading-9 tracking-wide text-gray-900">
+                        Create an Account
+                    </h2>
+                </div>
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form
+                        className="space-y-6"
+                        action={process.env.REACT_APP_ADD_USER_ENDPOINT}
+                        onSubmit={handleSubmit}
+                        method="POST">
+                        <div>
+                            <label htmlFor="firstName" className="block text-sm font-medium leading-6 text-gray-900">
+                                First Name
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="firstName"
+                                    name="firstName"
+                                    type="text"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-gray-900">
+                                Last Name
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="lastName"
+                                    name="lastName"
+                                    type="text"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+                        <div>
+
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                Email address
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Password
+                                </label>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+                            >
+                                Sign Up
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
 }
