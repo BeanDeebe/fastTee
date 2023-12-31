704e180f40c9618724c8fa6b6c746660f5c1d701
Dean Beebe <beebe.d@northeastern.edu>
2023-12-31 18:26:21 -0500

    User now successfully is created in database on submit

--- fastTeeBackend/src/main/java/com/example/fasttee/controller/UserController.java
+++ fastTeeBackend/src/main/java/com/example/fasttee/controller/UserController.java
@@ -5,11 +5,13 @@ import com.example.fasttee.models.UserModel;
 import com.example.fasttee.repositories.TeeTimeRepository;
 import com.example.fasttee.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping(value="/api/user")
 public class UserController {
@@ -25,8 +27,9 @@ public class UserController {
 
     @PostMapping(value = "/add")
     public String addUser(@RequestBody UserModel userModel) {
+        userModel.setBookedTeeTimes(new ArrayList<>());
         userRepository.save(userModel);
-        return "User created!";
+        return "User Created";
     }
 
     @PostMapping(value = "/{id}/bookTeeTime")
