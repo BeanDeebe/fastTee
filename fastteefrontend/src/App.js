2387ce5688518965e26d57e8f30164169f94cda6
Dean Beebe <beebe.d@northeastern.edu>
2023-12-31 16:18:08 -0500

    Welcome page basics setup. Buttons do not currently go anywhere.

--- fastteefrontend/src/App.js
+++ fastteefrontend/src/App.js
@@ -1,8 +1,9 @@
-import "./App.css";
+import Welcome from "./Pages/Welcome/Welcome";
+
 function App() {
   return (
       <div>
-        <h1>Hello World</h1>
+        <Welcome />
       </div>
  );
 }
